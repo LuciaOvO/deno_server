32b58a2915f1d2a0b7084afd4bc92fe0544d1b3b
LuciaOvO <s3696963@student.rmit.edu.au>
2022-10-07 12:18:34 +1100

    1

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ import { serveDir } from "https://deno.land/std@0.157.0/http/file_server.ts"
 
 serve (handler, { port: 80 })
 
-function handler (req) {
+function handler (incoming_req) {
 
-    const options = {
-        fsRoot: `public`
+    let req = incoming_req
+
+    const upgrade = req.headers.get ("upgrade") || ""
+
+    // check if it is an upgrade request
+    if (upgrade.toLowerCase() == "websocket") {
+        const { socket, response } = Deno.upgradeWebSocket (req)
+        return response
     }
 
     if (req.url.endsWith (`/`)) {
-        const aug_req = new Request (`${ req.url }index.html`, req)
-        return serveDir (aug_req, options)
+        req = new Request (`${ req.url }index.html`, req)
+    }
+
+    const options = {
+        fsRoot: `public`
     }
 
     return serveDir (req, options)
